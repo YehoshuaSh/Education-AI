09525f2b5208c275561a71921cf003facc5a1a32
Avshalom <avshalom.tam@gmail.com>
2020-07-31 12:55:01 +0300

    prepared connection to database

--- src/utils/db_req.js
+++ src/utils/db_req.js
@@ -1,7 +1,10 @@
 const stat = (course, lecture) => {
-    var lect_num = parseInt(lecture) + 20
+    var lect_num = parseInt(lecture)
 
-    const sentence = 'the percent for course ' + course + ' in lecture number ' + lect_num + ' is 23' 
+    // TODO make connection to db
+    const percent_from_db = Math.random().toFixed(2)*100
+    
+    const sentence = 'the percent for course ' + course + ' in lecture number ' + lect_num + ' is ' + percent_from_db 
     return sentence
 }
 
